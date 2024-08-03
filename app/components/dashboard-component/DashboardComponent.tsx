d0e90019c6655f7d2e645bb6c096a48d9ab7978e
franklin-raph <igboekwulusifranklin@gmail.com>
2024-08-03 11:15:56 +0100

    update

--- app/components/dashboard-component/DashboardComponent.tsx
+++ app/components/dashboard-component/DashboardComponent.tsx
@@ -12,16 +12,18 @@ const DashboardComponent = () => {
 
   const router = useRouter()
 
-  // const {data, isLoading, isError} = useQuery({
-  //   queryKey: ['users'],
-  //   queryFn: async () => {
-  //     const { data } = await api.get('/users/get-users/student')
-  //     return data
-  //   }
-  // })
+  const {data, isLoading, isError} = useQuery({
+    queryKey: ['users'],
+    queryFn: async () => {
+      const { data } = await api.get('/summary')
+      console.log(data);
+      
+      return data.data
+    }
+  })
   
-  // if (isLoading) return <PageLoader />;
-  // if (isError) return <div>Sorry There was an Error</div>
+  if (isLoading) return <PageLoader />;
+  if (isError) return <div>Sorry There was an Error</div>
 
 
   return (
@@ -46,39 +48,89 @@ const DashboardComponent = () => {
                       <img src="./images/buildings.svg" alt="" />
                     </div>
                   </div>
-                  <p className="font-[600] text-text-color text-[24px]">250</p>
-                  <div className="flex items-center mt-2">
-                    <img src="./images/arrow-up.svg" alt="" />
-                    <p className="text-[#25751E] text-[12px]">+15% this week</p>
-                  </div>
+                  <p className="font-[600] text-text-color text-[24px]">{data.totalOrgan}</p>
                 </div>
 
                 <div className="bg-[#F7F7F7] p-3 rounded-[20px]">
                   <div className="flex items-center justify-between mb-5">
-                    <p className="text-[#4F4F4F] font-[600]">Active subscription</p>
+                    <p className="text-[#4F4F4F] font-[600]">Active Plans</p>
                     <div className='bg-[#23AEAE] p-2 rounded-full'>
                       <img src="./images/card-tick.svg" alt="" />
                     </div>
                   </div>
-                  <p className="font-[600] text-text-color text-[24px]">250</p>
-                  <div className="flex items-center mt-2">
-                    <img src="./images/arrow-up.svg" alt="" />
-                    <p className="text-[#25751E] text-[12px]">+15% this week</p>
-                  </div>
+                  <p className="font-[600] text-text-color text-[24px]">{data?.activePlans}</p>
                 </div>
 
                 <div className="bg-[#F7F7F7] p-3 rounded-[20px]">
                   <div className="flex items-center justify-between mb-5">
-                    <p className="text-[#4F4F4F] font-[600]">Subscription revenue</p>
+                    <p className="text-[#4F4F4F] font-[600]">Total Revenue</p>
                     <div className='bg-[#23AEAE] p-2 rounded-full'>
                       <img src="./images/card-tick.svg" alt="" />
                     </div>
                   </div>
-                  <p className="font-[600] text-text-color text-[24px]">#250,000,000</p>
-                  <div className="flex items-center mt-2">
-                    <img src="./images/arrow-up.svg" alt="" />
-                    <p className="text-[#25751E] text-[12px]">+15% this week</p>
+                  <p className="font-[600] text-text-color text-[24px]">#{data?.totalRevenue}</p>
+                </div>
+              </div>
+              <div className="grid grid-cols-3 gap-5 px-5 py-5 rounded-[12px]">
+                <div className="bg-[#F7F7F7] p-3 rounded-[20px]">
+                  <div className="flex items-center justify-between mb-5">
+                    <p className="text-[#4F4F4F] font-[600]">Total AmountBasic Plan</p>
+                    <div className='bg-[#23AEAE] p-2 rounded-full'>
+                      <img src="./images/buildings.svg" alt="" />
+                    </div>
                   </div>
+                  <p className="font-[600] text-text-color text-[24px]">#{data?.totalAmountBasicPlan}</p>
+                </div>
+
+                <div className="bg-[#F7F7F7] p-3 rounded-[20px]">
+                  <div className="flex items-center justify-between mb-5">
+                    <p className="text-[#4F4F4F] font-[600]">Total Amount Bulk Plan</p>
+                    <div className='bg-[#23AEAE] p-2 rounded-full'>
+                      <img src="./images/card-tick.svg" alt="" />
+                    </div>
+                  </div>
+                  <p className="font-[600] text-text-color text-[24px]">#{data?.totalAmountBulkPlan}</p>
+                </div>
+
+                <div className="bg-[#F7F7F7] p-3 rounded-[20px]">
+                  <div className="flex items-center justify-between mb-5">
+                    <p className="text-[#4F4F4F] font-[600]">Total Amount Combo Plan</p>
+                    <div className='bg-[#23AEAE] p-2 rounded-full'>
+                      <img src="./images/card-tick.svg" alt="" />
+                    </div>
+                  </div>
+                  <p className="font-[600] text-text-color text-[24px]">#{data?.totalAmountComboPlan}</p>
+                </div>
+              </div>
+              <div className="grid grid-cols-3 gap-5 px-5 py-5 rounded-[12px]">
+                <div className="bg-[#F7F7F7] p-3 rounded-[20px]">
+                  <div className="flex items-center justify-between mb-5">
+                    <p className="text-[#4F4F4F] font-[600]">Sub Revenue</p>
+                    <div className='bg-[#23AEAE] p-2 rounded-full'>
+                      <img src="./images/buildings.svg" alt="" />
+                    </div>
+                  </div>
+                  <p className="font-[600] text-text-color text-[24px]">#{data?.subRevenue}</p>
+                </div>
+
+                <div className="bg-[#F7F7F7] p-3 rounded-[20px]">
+                  <div className="flex items-center justify-between mb-5">
+                    <p className="text-[#4F4F4F] font-[600]">Total Amount Contract</p>
+                    <div className='bg-[#23AEAE] p-2 rounded-full'>
+                      <img src="./images/card-tick.svg" alt="" />
+                    </div>
+                  </div>
+                  <p className="font-[600] text-text-color text-[24px]">#{data?.totalAmountContract}</p>
+                </div>
+
+                <div className="bg-[#F7F7F7] p-3 rounded-[20px]">
+                  <div className="flex items-center justify-between mb-5">
+                    <p className="text-[#4F4F4F] font-[600]">Total Amount Result Plan</p>
+                    <div className='bg-[#23AEAE] p-2 rounded-full'>
+                      <img src="./images/card-tick.svg" alt="" />
+                    </div>
+                  </div>
+                  <p className="font-[600] text-text-color text-[24px]">#{data?.totalAmountResultPlan}</p>
                 </div>
               </div>
             </div>
--- app/components/manage-users-component/ManageUsersComponents.tsx
+++ app/components/manage-users-component/ManageUsersComponents.tsx
@@ -16,7 +16,7 @@ const ManageUsersComponents = () => {
     const router = useRouter()
 
     const getAllSubs = async () => {
-        const { data } = await api.get('/subscriptions')
+        const { data } = await api.get('/organizations')
         console.log(data);
         return data.data
     }
@@ -34,13 +34,13 @@ const ManageUsersComponents = () => {
                 <TopNav />
                 <div className="flex items-center justify-between mb-[1rem] px-[30px] py-[1rem] bg-[#F2FCF7]">
                     <p className="text-[28px] text-primary-color font-[600]">Manage users</p>
-                    <div className="flex items-center gap-3">
+                    {/* <div className="flex items-center gap-3">
                         <button  className="bg-white text-[#2D3934] px-4 py-3 rounded-[8px] text-[14px] flex items-center gap-2 font-[600]">
                             <CiFilter className="text-[#2D3934] text-[14px]"/>
                             <p className='border-l pl-3'>Filter</p>
                             <GoChevronDown className="text-[#2D3934] text-[14px] ml-2"/>
                         </button>
-                    </div>
+                    </div> */}
                 </div>
                 <div className="relative overflow-x-auto">
                     <table className="w-full text-sm text-left rtl:text-left">
@@ -51,7 +51,7 @@ const ManageUsersComponents = () => {
                                 <th scope="col" className="px-6 py-3 font-[700]">Email</th>
                                 <th scope="col" className="px-6 py-3 font-[700]">Phone</th>
                                 <th scope="col" className="px-6 py-3 font-[700]">Date Joined</th>
-                                <th scope="col" className="px-6 py-3 font-[700]">Action</th>
+                                {/* <th scope="col" className="px-6 py-3 font-[700]">Action</th> */}
                             </tr>
                         </thead>
                         <tbody>
@@ -60,13 +60,13 @@ const ManageUsersComponents = () => {
                                     return(
                                         <tr style={{borderBottom:"1px solid #dcdcdc"}} key={index}>
                                             <td className="px-6 py-4">{index +1}</td>
-                                            <td className="px-6 py-4">{plan.name}</td>
-                                            <td className="px-6 py-4">{plan.duration}</td>
-                                            <td className="px-6 py-4">{plan.planValidity}</td>
-                                            <td className="px-6 py-4">{plan.planValidity}</td>
-                                            <td className="px-6 py-4">
+                                            <td className="px-6 py-4">{plan.nameOfEstablishment}</td>
+                                            <td className="px-6 py-4">{plan.email}</td>
+                                            <td className="px-6 py-4">{plan.phone}</td>
+                                            <td className="px-6 py-4">{`${plan.createdAt}`.slice(0, 10)}</td>
+                                            {/* <td className="px-6 py-4">
                                                 <button onClick={() => router.replace(`subscription/${plan._id}`)} className='bg-gray-400 px-3 py-2 rounded-[5px] text-white'>View</button>
-                                            </td>
+                                            </td> */}
                                         </tr>
                                     )
                                 })
